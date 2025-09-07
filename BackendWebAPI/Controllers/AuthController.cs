4f700304efca392d5647c629c63ccc9901b771af
xuckless <xuckless@gmail.com>
2025-09-07 01:04:52 -0400

    added instructions

--- BackendWebAPI/Controllers/AuthController.cs
+++ BackendWebAPI/Controllers/AuthController.cs
@@ -2,5 +2,9 @@ namespace BackendWebAPI.Controllers;
 
 public class AuthController
 {
-    
+    // Add all the REST API/s of the Controller 
+    // ISSUE JWT 
+    // 2-3 other functions 
+    // Login request
+    // Authorise request 
 }
--- TicTacToeUltimate.Shared/Models/AuthDto.cs
+++ TicTacToeUltimate.Shared/Models/AuthDto.cs
@@ -2,5 +2,7 @@ namespace TicTacToeUltimate.Shared.Models;
 
 public class AuthDto
 {
-    
+    // Login request
+    // Answer / Login result 
+    // 2 models to send and receive the API response
 }
